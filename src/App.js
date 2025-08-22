645826bf8e18b0f9ffa447918abde8cc3abde5cd
QudratUllahKhan04 <qudratkhan.creator@gmail.com>
2025-08-22 01:38:51 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const UserDetail = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch(`https://verification-backend.vercel.app/${id}`);
+        const res = await fetch(`https://verification-backend-three.vercel.app/${id}`);
         if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
         const data = await res.json();
         console.log("✅ Data received:", data);
