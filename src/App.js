f2482b58a9a0bf9d78040c8f98573cfc0f1daf74
QudratUllahKhan04 <qudratkhan.creator@gmail.com>
2025-12-11 08:20:48 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -48,12 +48,22 @@ const UserDetail = () => {
         <div className="data-card-wrapper">
           <div className="data-card-body">
             <div className="data-row"><strong>Deliverable Id :</strong><span>{data.deliverableId}</span></div>
-            <div className="data-row"><strong>Published on :</strong><span>{data.publishedOn}</span></div>
+           <div className="data-row">
+  <strong>Published on :</strong>
+  <span>{formatDate(data.publishedOn)}</span>
+</div>
             <div className="data-row"><strong>QR Code Status :</strong><span>{data.qrCodeStatus}</span></div>
             <div className="data-row"><strong>NAME :</strong><span>{data.name}</span></div>
             <div className="data-row"><strong>ID :</strong><span>{data.userId}</span></div>
-            <div className="data-row"><strong>ISSUED ON :</strong><span>{data.issuedOn}</span></div>
-            <div className="data-row"><strong>VALID UNTIL :</strong><span>{data.validUntil}</span></div>
+<div className="data-row">
+  <strong>ISSUED ON :</strong>
+  <span>{formatDate(data.issuedOn)}</span>
+</div>
+
+<div className="data-row">
+  <strong>VALID UNTIL :</strong>
+  <span>{formatDate(data.validUntil)}</span>
+</div>
             <div className="data-row"><strong>TYPE :</strong><span>{data.type}</span></div>
             <div className="data-row"><strong>MODEL :</strong><span>{data.model}</span></div>
             <div className="data-row"><strong>COMPANY :</strong><span>{data.company}</span></div>
